b2898654fa53b98f42cbffc183d972bce569e56d
nichmomanyi <nmomce44@gmail.com>
2023-12-09 19:36:01 +0300

    Day 2

--- day_1.py
+++ day_1.py
@@ -9,3 +9,5 @@ print("Your band name could be " + city + " " + pet)
 #5. Make sure the input cursor shows on a new line:
 
 # Solution: https://replit.com/@appbrewery/band-name-generator-end
+a = int("5") / int(2.7)
+print (a)

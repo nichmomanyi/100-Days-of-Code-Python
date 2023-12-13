9de9f75f1029b9a71b2ccb2dc90ae6d6186b0c24
nichmomanyi <nmomce44@gmail.com>
2023-12-13 11:53:04 +0300

    day 3

--- Day_2.py
+++ Day_2.py
@@ -13,4 +13,4 @@ bill_per_person = total_bill / people
 final_amount = round(bill_per_person, 2)
 
 
-print(f"Each person should pay: ${final_amount}")
+print(f"Each person should make payment of: ${final_amount}")

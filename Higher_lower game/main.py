43fe58198a24a6ce36bf5e0d95defd0d4b672c4d
nichmomanyi <nmomce44@gmail.com>
2024-01-08 10:04:15 +0300

    Higher Lower Game

--- Higher_lower game/main.py
+++ Higher_lower game/main.py
@@ -21,36 +21,32 @@ def check_answer(guess, a_followers, b_followers):
     else:
         return guess == "b"
 
-#Generate a random account for the game data
-account_a = random.choice(data)
-account_b = random.choice(data)
-if account_b == account_a:
-    account_b = random.choice(data)
-print(f"Compare A: {format_data(account_a)}")
-print(vs)
-print(f"Against B: {format_data(account_b)}")
-
-
 score = 0
-#Ask for user a guess
-guess = input("Who has more followers? Type 'A' or 'B': ").lower()
-#check if user is correct
-a_follower_count = account_a["follower_count"]
-b_follower_count = account_b["follower_count"]
-check_answer()
-##use if statement to check if user is correct
-is_correct = check_answer(guess, a_follower_count, b_follower_count)
-#Give user feedback on their guess
-if is_correct:
-    score  += 1
-    print(f"You're right!. Current score: {score}")
-else:
-    print(f"Sorry, that's wrong. Final score: {score}")
+game_should_continue = True
+account_b = random.choice(data)
 
-#Score keeping
+while game_should_continue:
+#Generate a random account for the game data
+    account_a = account_b
+    account_b = random.choice(data)
+    if account_b == account_a:
+        account_b = random.choice(data)
+    print(f"Compare A: {format_data(account_a)}")
+    print(vs)
+    print(f"Against B: {format_data(account_b)}")
 
-#Make the game repeatable
 
-#Making account at position B become the next position A
-
-#Clear the screen between rounds
+    #Ask for user a guess
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+    #check if user is correct
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+    ##use if statement to check if user is correct
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+    #Give user feedback on their guess
+    if is_correct:
+        score  += 1
+        print(f"You're right!. Current score: {score}")
+    else:
+        game_should_continue = False
+        print(f"Sorry, that's wrong. Final score: {score}")

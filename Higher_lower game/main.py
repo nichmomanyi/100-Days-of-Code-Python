4b8622d54bd30fdc47eda32620be20d4f6c17638
nichmomanyi <nmomce44@gmail.com>
2024-01-08 09:40:08 +0300

    Higher Lower Game

--- Higher_lower game/main.py
+++ Higher_lower game/main.py
@@ -13,6 +13,14 @@ def format_data(account):
     country = account["country"]
     return f"{name}, a {description}, from {country}"
 
+##Get follower count on each account
+def check_answer(guess, a_followers, b_followers):
+    """Checks followers against user's guess and returns True if they got it right Or False if they got it wrong.""" 
+    if a_followers > b_followers:
+        return guess == "a"
+    else:
+        return guess == "b"
+
 #Generate a random account for the game data
 account_a = random.choice(data)
 account_b = random.choice(data)
@@ -23,13 +31,21 @@ print(vs)
 print(f"Against B: {format_data(account_b)}")
 
 
+score = 0
 #Ask for user a guess
-
+guess = input("Who has more followers? Type 'A' or 'B': ").lower()
 #check if user is correct
-##Get follower count on each account
+a_follower_count = account_a["follower_count"]
+b_follower_count = account_b["follower_count"]
+check_answer()
 ##use if statement to check if user is correct
-
+is_correct = check_answer(guess, a_follower_count, b_follower_count)
 #Give user feedback on their guess
+if is_correct:
+    score  += 1
+    print(f"You're right!. Current score: {score}")
+else:
+    print(f"Sorry, that's wrong. Final score: {score}")
 
 #Score keeping
 

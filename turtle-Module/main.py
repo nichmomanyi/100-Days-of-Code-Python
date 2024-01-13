00bf644c52a295f6ffd65d33ebd2ed26c0e530e3
nichmomanyi <nmomce44@gmail.com>
2024-01-13 14:00:58 +0300

    Quiz game

---
+++ turtle-Module/main.py
@@ -0,0 +1,18 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("green")
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()

5298756054c6d11ec16c4aa4400cdc11d8196d56
nichmomanyi <nmomce44@gmail.com>
2024-01-15 01:33:28 +0300

    Quiz game

--- turtle-Module/main.py
+++ turtle-Module/main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 
 tim = Turtle()
+
 # timmy_the_turtle.shape("turtle")
 # timmy_the_turtle.color("blue")
 # timmy_the_turtle.right(60)
@@ -14,12 +15,20 @@ tim = Turtle()
 #     tim.forward(10)
 #     tim.color('black')
 
-for _ in range(10):
-    tim.forward(10)
-    tim.penup()
-    tim.forward(10)
-    tim.pendown()
+# for _ in range(10):
+#     tim.forward(10)
+#     tim.penup()
+#     tim.forward(10)
+#     tim.pendown()
 
+
+num_sides = 3
+while num_sides <=10:
+    num_sides += 1
+    for _ in range (num_sides):
+        angle = 360 / num_sides
+        tim.forward(100)
+        tim.right(angle)
 
 
 

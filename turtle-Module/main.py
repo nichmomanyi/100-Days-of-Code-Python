4707fffd6ad324db577558df0954069ae706a2f8
nichmomanyi <nmomce44@gmail.com>
2024-01-14 06:01:10 +0300

    Quiz game

--- turtle-Module/main.py
+++ turtle-Module/main.py
@@ -1,12 +1,24 @@
 from turtle import Turtle, Screen
 
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("blue")
+tim = Turtle()
+# timmy_the_turtle.shape("turtle")
+# timmy_the_turtle.color("blue")
+# timmy_the_turtle.right(60)
+# for _ in range(4):
+#     tim.forward(100)
+#     tim.right(90)
 
+# for _ in range(10):
+#     tim.forward(10)
+#     tim.color('white')
+#     tim.forward(10)
+#     tim.color('black')
 
-
-
+for _ in range(10):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
 
 
 

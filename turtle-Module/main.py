de5b147524418b01d96f607cc22b638eb0da2b62
nichmomanyi <nmomce44@gmail.com>
2024-01-15 06:03:48 +0300

    turtle intro

--- turtle-Module/main.py
+++ turtle-Module/main.py
@@ -1,6 +1,8 @@
+import turtle
 from turtle import Turtle, Screen
 
 tim = Turtle()
+import random
 
 # timmy_the_turtle.shape("turtle")
 # timmy_the_turtle.color("blue")
@@ -21,18 +23,41 @@ tim = Turtle()
 #     tim.forward(10)
 #     tim.pendown()
 
+# num_sides = 2
+# while num_sides <=10:
+#     num_sides += 1
+#     for _ in range (num_sides):
+#         angle = 360 / num_sides
+#         tim.forward(100)
+#         tim.right(angle)
 
-num_sides = 3
-while num_sides <=10:
-    num_sides += 1
-    for _ in range (num_sides):
-        angle = 360 / num_sides
-        tim.forward(100)
-        tim.right(angle)
+turtle.colormode(255)
 
 
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
 
 
+# directions = [0, 90, 180, 270]
+# tim.pensize(10)
+# tim.speed('fastest')
+#
+# for _ in range(200):
+#     tim.forward(30)
+#     tim.setheading(random.choice(directions))
+#     tim.color(random_color())
+
+tim.speed("fastest")
+def spiro(gap):
+    for _ in range(int(360/gap)):
+        tim.color(random_color())
+        tim.circle(50)
+        tim.setheading(tim.heading() + gap)
+spiro(5)
 
 
 screen = Screen()

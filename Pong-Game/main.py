76fee0d5ba3019bf7d1ee6878bb597af945bfdef
nichmomanyi <nmomce44@gmail.com>
2024-01-24 08:03:03 +0300

    Pong game

--- Pong-Game/main.py
+++ Pong-Game/main.py
@@ -1,22 +1,24 @@
 from turtle import Turtle, Screen
+from paddle import Paddle
 
 screen = Screen()
-
 
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("PONG")
+screen.tracer(0)
 
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(350, 0)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
-def go_up():
-    new_y = paddle.ycor() + 20
-
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()

8114e93e0b3e77b3ed94989aae57906d3bff261a
nichmomanyi <nmomce44@gmail.com>
2024-01-26 11:16:45 +0300

    open, read, write

---
+++ open-read-write-file/main.py
@@ -0,0 +1,9 @@
+# with open("my_file.txt") as file:
+#     content = file.read()
+#     print(content)
+
+with open ("my_file.txt", "w") as file:
+    file.write("\n My name is Nic")
+
+with open ("my_file.txt", "w") as file:
+    file.write("Who are you")

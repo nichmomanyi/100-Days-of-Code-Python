86f932485e94c7ea9b0ce621f6a27702eea5b488
nichmomanyi <nmomce44@gmail.com>
2024-01-31 05:07:30 +0300

    US states naming

--- US_States naming game/main.py
+++ US_States naming game/main.py
@@ -10,17 +10,15 @@ turtle.shape(Image)
 data = pd.read_csv("50_states.csv")
 all_states = data["state"].to_list()
 guessed_states = []
-missing_state = []
+
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States correct", prompt="What's another state name?").title()
 
     if answer_state == "Exit":
         # States not mentioned
-        for state in all_states:
-            if state not in guessed_states:
-                missing_state.append(state)
-            new_data = pd.DataFrame(missing_state)
-            new_data.to_csv("Staes to learn.csv")
+        missing_state = [state for state in all_states if state not in guessed_states]
+        new_data = pd.DataFrame(missing_state)
+        new_data.to_csv("States to learn.csv")
     if answer_state in all_states:
         guessed_states.append(answer_state)
         t = turtle.Turtle()

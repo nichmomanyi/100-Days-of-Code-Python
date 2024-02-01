91ff3faa76ff20f215644da764937c71b0d2af22
nichmomanyi <nmomce44@gmail.com>
2024-02-01 08:10:18 +0300

    Day 2

--- Day_2.py
+++ Day_2.py
@@ -3,3 +3,20 @@
 
 # news = [x*2 for x in range(1,5)]
 # print(news)
+def add(*numbers):
+    sum=0
+    for n in numbers:
+        sum+=n
+    print (sum)
+
+add(3,5,6,4)
+
+def calculate(**math):
+    print(math)
+    # for key, value in math.items():
+    #     #print(key)
+    #     print(value)
+    print(math["add"])
+
+
+calculate(add = 5, multiply = 6)

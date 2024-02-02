0dbf91c29138bb83342a08124c99ef0c4a4979ac
nichmomanyi <nmomce44@gmail.com>
2024-02-02 07:19:45 +0300

    TKINTER

--- TKinter_miles to kilometeer converter/main.py
+++ TKinter_miles to kilometeer converter/main.py
@@ -6,31 +6,31 @@ window.minsize(width=300, height=200)
 window.config(pady=20, padx=20)
 
 # Entry
-mile_entry = Entry()
-mile_entry.insert(END, string="0")
+mile_entry = Entry(width=7)
 mile_entry.grid(column=1, row=0)
 
-km_entry = Entry()
-km_entry.insert(END, string="0")
-km_entry.grid(column=1, row=1)
+km_input = Label(text="0")
+km_input.grid(column=1, row=1)
 
 # Label
-is_equal = Label(text="is equal to", font=("arial", 16, "normal"))
+is_equal = Label(text="is equal to")
 is_equal.grid(column=0, row=1)
 
-miles = Label(text="miles", font=("arial", 16, "normal"))
-miles.grid(column=2, row=0)
+miles_input = Label(text="miles")
+miles_input.grid(column=2, row=0)
 
-km = Label(text="Km", font=("arial", 16, "normal"))
-km.grid(column=2, row=1)
+km_lab = Label(text="Km")
+km_lab.grid(column=2, row=1)
 
 
 def converter():
-    pass
+    miles = float(mile_entry.get())
+    km = round(miles * 1.609, 2)
+    km_input.config(text=f"{km}")
 
 
 # button
-calc = Button(text="Calculate", command=converter())
+calc = Button(text="Calculate", command=converter)
 calc.grid(column=1, row=2)
 
 

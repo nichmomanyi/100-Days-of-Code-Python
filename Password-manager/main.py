c03853ff3fa0da1d149a031e648af3dcbf03b697
nichmomanyi <nmomce44@gmail.com>
2024-02-06 15:09:31 +0300

    Password manager updated

--- Password-manager/main.py
+++ Password-manager/main.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 from random import choice, randint, shuffle
 import json
 
+
 # import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -31,7 +32,7 @@ def save():
     email = email_entry.get()
     password = pass_entry.get()
     new_data = {
-        website:{
+        website: {
             "email": email,
             "password": password
         }
@@ -59,6 +60,22 @@ def save():
             pass_entry.delete(0, END)
 
 
+# ---------------------------- SEARCH PASSWORD ------------------------------- #
+def search_pass():
+    website = web_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No data file found.")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\n Password: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website}")
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
@@ -76,26 +93,29 @@ website_lab.grid(row=1, column=0)
 email_lab = Label(text="Email/Username:")
 email_lab.grid(row=2, column=0)
 
-password_lab = Label(text="Password: ")
+password_lab = Label(text="Password:")
 password_lab.grid(row=3, column=0)
 
 # Entries
-web_entry = Entry(width=36)
-web_entry.grid(column=1, row=1, columnspan=2)
+web_entry = Entry(width=30)
+web_entry.grid(column=1, row=1)
 web_entry.focus()
 
-email_entry = Entry(width=36)
+email_entry = Entry(width=47)
 email_entry.grid(column=1, row=2, columnspan=2)
 email_entry.insert(0, "nmomce44@gmail.com")
 
-pass_entry = Entry(width=18)
+pass_entry = Entry(width=27)
 pass_entry.grid(column=1, row=3)
 
 # Buttons
 gen_pass_button = Button(text="Generate Password", command=generate_password)
 gen_pass_button.grid(column=2, row=3, columnspan=2)
 
-add_button = Button(text="Add", width=30, command=save)
+add_button = Button(text="Add", width=45, command=save)
 add_button.grid(column=1, row=4, columnspan=2)
+
+search_button = Button(text="Search", command=search_pass, width=15)
+search_button.grid(column=2, row=1, columnspan=2)
 
 window.mainloop()

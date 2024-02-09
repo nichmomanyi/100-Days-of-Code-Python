843bc93825d1b49a528d4cd034457d28aab57054
nichmomanyi <nmomce44@gmail.com>
2024-02-09 06:43:54 +0300

    SMTPLIB

--- Birthday-wisher-SMTP/main.py
+++ Birthday-wisher-SMTP/main.py
@@ -1,11 +1,27 @@
 import smtplib
+import datetime as dt
+import  random
 
 my_email = "nmomce44@gmail.com"
 password = "vntfgxmruewsfcqy"
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(from_addr=my_email,
-                        to_addrs="tharry61@yahoo.com",
-                        msg="subject: Hi Python study\n\n Whats new with python")
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 4:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+    print(quote)
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs="tharry61@yahoo.com",
+                            msg=f"subject: FRIDAY MOTIVATION\n\n {quote}")
+
+
+
+
+
+
+

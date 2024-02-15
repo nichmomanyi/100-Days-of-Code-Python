67b59710405f0b009d54bd53a08370a9e88df0b3
nichmomanyi <nmomce44@gmail.com>
2024-02-15 06:46:25 +0300

    ISS Overhead Notifier Project

--- API endponts and requests/api_par.py
+++ API endponts and requests/api_par.py
@@ -12,11 +12,13 @@ parameters = {
 
 response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
-
 data = response.json()
 
-sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
-sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+iss_latitude = float(data["iss_position"]["latitude"])
+iss_longitude = float(data["iss_position"]["longitude"])
+
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
 print(sunrise)
 print(sunset)

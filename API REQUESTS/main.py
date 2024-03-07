256ec8e6a5602c99bcb5d8ea074d0e0cd7899ee7
nichmomanyi <nmomce44@gmail.com>
2024-03-07 19:46:50 +0300

    Habit tracking project

---
+++ API REQUESTS/main.py
@@ -0,0 +1,47 @@
+import requests
+from datetime import datetime
+
+TOKEN = "gjfdkwkkffj"
+USERNAME = "nich254"
+GRAPHID = "graph1"
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_param = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+# response = requests.post(pixela_endpoint, json=user_param)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_param = {
+    "id": GRAPHID,
+    "name": "Cycling Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "ajisai"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# response = requests.post(graph_endpoint, json=graph_param, headers=headers)
+# print(response.text)
+
+pixel_endpoint = f"{graph_endpoint}/{GRAPHID}"
+
+date = datetime.today()
+updated_date = date.strftime("%Y%m%d")
+print(updated_date)
+
+pixel_param = {
+    "date" : updated_date,
+    "quantity": "50"
+}
+
+response = requests.post(pixel_endpoint, json=pixel_param, headers=headers)
+print(response.text)

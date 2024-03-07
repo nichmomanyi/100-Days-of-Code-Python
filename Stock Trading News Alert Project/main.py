e0f2f58def3cd116eb29f887ead92aed37581aaa
nichmomanyi <nmomce44@gmail.com>
2024-03-07 08:39:47 +0300

    Stock trading alert project

--- Stock Trading News Alert Project/main.py
+++ Stock Trading News Alert Project/main.py
@@ -1,12 +1,12 @@
 import requests
 
 STOCK_NAME = "TSLA"
-COMPANY_NAME = "Tesla Inc"
+COMPANY_NAME = "Tesla inc"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
-STOCK_API_KEY = "DRD4ZM8FY8S6XMPN"
+STOCK_API_KEY = "V4T499YXU4UFIPXI"
 NEWS_API_KEY = "91a92780e73b4c9fab89fc5d6fc69ffb"
 
 # Get yesterday's closing stock price.
@@ -18,45 +18,47 @@ stock_params = {
 
 response = requests.get(STOCK_ENDPOINT, params=stock_params)
 data = response.json()["Time Series (Daily)"]
-#Data dictionary to list
+# Data dictionary to list
 data_list = [value for (key, value) in data.items()]
 yesterday_data = data_list[0]
 yesterday_closing_data = yesterday_data["4. close"]
 print(yesterday_closing_data)
 
-#Day before yesterday's closing stock price
+# Day before yesterday's closing stock price
 dey_before_yesterday_data = data_list[1]
 day_before_yesterday_closing_data = dey_before_yesterday_data["4. close"]
 print(day_before_yesterday_closing_data)
 
-#Find the positive difference between 1 and 2
+# Find the positive difference between 1 and 2
 difference = abs(float(yesterday_closing_data) - float(day_before_yesterday_closing_data))
 print((difference))
 
-#The percentage difference
-percentage_difference = (difference/float(yesterday_closing_data))*100
+# The percentage difference
+percentage_difference = (difference / float(yesterday_closing_data)) * 100
 print(percentage_difference)
 
-if percentage_difference > 5:
-    print("Get News")
+# Use the News API to get articles related to the COMPANY_NAME.
+news_param = {
+    "apiKey":NEWS_API_KEY,
+    "qInTitle": COMPANY_NAME
+}
 
-    ## STEP 2: https://newsapi.org/
-    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+if percentage_difference > 0.1:
+    news_response = requests.get(NEWS_ENDPOINT, params=news_param)
+    articles = news_response.json()["articles"]
+    print(articles)
 
-#TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
+# TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
 
-#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
+## STEP 3: Use twilio.com/docs/sms/quickstart/python
+# to send a separate message with each article's title and description to your phone number.
 
-    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
-    #to send a separate message with each article's title and description to your phone number.
+# TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
 
-#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
-
-#TODO 9. - Send each article as a separate message via Twilio.
+# TODO 9. - Send each article as a separate message via Twilio.
 
 
-
-#Optional TODO: Format the message like this:
+# Optional TODO: Format the message like this:
 """
 TSLA: 🔺2%
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
@@ -66,4 +68,3 @@ or
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
 Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
 """
-

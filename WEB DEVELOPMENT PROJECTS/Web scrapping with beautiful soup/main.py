afc50e81681dc88b411f21290c7beae1115aa2c1
nichmomanyi <nmomce44@gmail.com>
2024-03-17 11:07:21 +0300

    web scrapping

--- WEB DEVELOPMENT PROJECTS/Web scrapping with beautiful soup/main.py
+++ WEB DEVELOPMENT PROJECTS/Web scrapping with beautiful soup/main.py
@@ -1,11 +1,20 @@
-from bs4 import  BeautifulSoup
+from bs4 import BeautifulSoup
 import lxml
 
 with open("website.html", encoding="utf-8") as file:
     content = file.read()
 
 soup = BeautifulSoup(content, "lxml")
-print(soup.title)
-print(soup.title.name)
-print(soup.a)
-print(soup.p)
+
+anchor_tags = soup.find_all(name="p")
+for tag in anchor_tags:
+    # print(tag.getText())
+    # print(tag.get("href"))
+    pass
+
+heading = soup.find(name= "h1", id="name" )
+print(heading)
+
+company_url = soup.select_one("p a")
+print(company_url)
+

f42fcb4ff3f89352f3a0c6bd731b8ac4c7ca617e
nichmomanyi <nmomce44@gmail.com>
2024-03-23 13:35:15 +0300

    top 100 movies (web scrapping)

--- 100 movies to watch start/main.py
+++ 100 movies to watch start/main.py
@@ -1,8 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+URL = "https://www.empireonline.com/movies/features/best-movies-2/"
 
-# Write your code below this line 👇
+response = requests.get(URL)
+website_html = response.text
+
+soup = BeautifulSoup(website_html, "html.parser")
+print(soup.prettify())
+all_movies = soup.find_all(name="h3",class_="listicleItem_listicle-item__title__BfenH")
+# print(all_movies)
+movie_titles = [movie.getText() for movie in all_movies]
+movies = movie_titles[::-1]
+with open("movies.txt", "w") as file:
+    for movie in movies:
+        file.write(f"{movie}\n")
 
 

6649e285378b1e1fb2a7bb0b15a2d643b56da305
nichmomanyi <nmomce44@gmail.com>
2024-03-23 11:43:42 +0300

    web scrapping

--- WEB DEVELOPMENT PROJECTS/Web scrapping with beautiful soup/main.py
+++ WEB DEVELOPMENT PROJECTS/Web scrapping with beautiful soup/main.py
@@ -1,20 +1,29 @@
+# Thanks to Charlie for this code
+# https://www.udemy.com/course/100-days-of-code/learn/#questions/19476080
+
 from bs4 import BeautifulSoup
-import lxml
+import requests
 
-with open("website.html", encoding="utf-8") as file:
-    content = file.read()
+response = requests.get("https://news.ycombinator.com/")
+yc_web_page = response.text
+soup = BeautifulSoup(yc_web_page, 'html.parser')
 
-soup = BeautifulSoup(content, "lxml")
+article_titles = []
+article_links = []
+for article_tag in soup.find_all(name="span", class_="titleline"):
+    article_titles.append(article_tag.getText())
+    article_links.append(article_tag.find("a")["href"])
 
-anchor_tags = soup.find_all(name="p")
-for tag in anchor_tags:
-    # print(tag.getText())
-    # print(tag.get("href"))
-    pass
+article_upvotes = []
+for article in soup.find_all(name="td", class_="subtext"):
+    if article.span.find(class_="score") is None:
+        article_upvotes.append(0)
+    else:
+        article_upvotes.append(int(article.span.find(class_="score").contents[0].split()[0]))
 
-heading = soup.find(name= "h1", id="name" )
-print(heading)
-
-company_url = soup.select_one("p a")
-print(company_url)
-
+max_points_index = article_upvotes.index(max(article_upvotes))
+print(
+    f"{article_titles[max_points_index]}, "
+    f"{article_upvotes[max_points_index]} points, "
+    f"available at: {article_links[max_points_index]}."
+)

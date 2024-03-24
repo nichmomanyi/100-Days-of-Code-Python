50ed73c17938ffcaac1155418478be1ae53f29f0
nichmomanyi <nmomce44@gmail.com>
2024-03-24 10:53:05 +0300

    interactions selenium web driver

--- Selenium Web driver/interaction.py
+++ Selenium Web driver/interaction.py
@@ -12,4 +12,4 @@ driver = webdriver.Chrome(r"C:\Users\user\chromedriver", options=chrome_options)
 driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
 article_driver = driver.find_element(By.CSS_SELECTOR, value="#articlecount a")
-print(f"There are: {article_driver.text} ")
+print(f"There are: {article_driver.text} articles")

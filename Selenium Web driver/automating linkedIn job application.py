7c9a30a97d9ca864ab433aa9e3fb9657c3951b8f
nichmomanyi <nmomce44@gmail.com>
2024-03-26 09:54:58 +0300

    interactions selenium web driver linkedIn

---
+++ Selenium Web driver/automating linkedIn job application.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(r"C:\Users\user\chromedriver", options=chrome_options)
+driver.get("https://www.linkedin.com/jobs/search/?f_LF=f_AL&geoId=102257491&keywords=python%20developer&location=London%2C%20England%2C%20United%20Kingdom&redirect=false&position=1&pageNum=0")

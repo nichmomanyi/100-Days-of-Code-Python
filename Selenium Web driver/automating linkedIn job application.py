3cfe20c637bf45602e232d99ae9c3e16c0a48dd8
nichmomanyi <nmomce44@gmail.com>
2024-03-27 13:13:23 +0300

    interactions selenium web driver linkedIn

--- Selenium Web driver/automating linkedIn job application.py
+++ Selenium Web driver/automating linkedIn job application.py
@@ -1,8 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(r"C:\Users\user\chromedriver", options=chrome_options)
 driver.get("https://www.linkedin.com/jobs/search/?f_LF=f_AL&geoId=102257491&keywords=python%20developer&location=London%2C%20England%2C%20United%20Kingdom&redirect=false&position=1&pageNum=0")
+
+# Click Reject Cookies Button
+time.sleep(2)
+reject_button = driver.find_element(by=By.CSS_SELECTOR, value='button[action-type="DENY"]')
+reject_button.click()

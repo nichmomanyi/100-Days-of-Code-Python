4adc7c1395e9fbb02e06636bd081e5c0ee672317
nichmomanyi <nmomce44@gmail.com>
2024-03-27 13:15:23 +0300

    interactions selenium web driver linkedIn

--- Selenium Web driver/automating linkedIn job application.py
+++ Selenium Web driver/automating linkedIn job application.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.common.keys import Keys
+import time
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
